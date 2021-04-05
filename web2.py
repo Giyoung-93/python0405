99aad82207ede68487de00ec6582edbc928d2eff
Giyoung-93 <zxzx0520@gmail.com>
2021-04-05 12:49:34 +0900

    웹툰 리스트까지 출력

--- web2.py
+++ web2.py
@@ -5,10 +5,23 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 #웹서버에 요청해서 결과물을 문자열로 받기
-data = urllib.request.urlopen('http://comic.naver.com/webtoon/list.nhn?title')
+data = urllib.request.urlopen('http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri')
 #검색이 용이한 객체
 soup = BeautifulSoup(data, "html.parser")
 
 # <td class="title">
 # 	<a href="/webtoon/detail.nhn">마음의 소리 49화 <지혜></a>
 # </td>
+#리시트 객체를 리턴(갯수 10개)
+cartoons = soup.find_all("td", class_="title")
+#첫번째만 슬라이싱(10개) cartoons[0], cartoons[1]...
+title = cartoons[0].find("a").text
+link = cartoons[0].find("a")["href"]
+print(title)
+print(link)
+
+#반복구문
+print("=====반복구문====")
+for item in cartoons:
+    title = item.find("a").text
+    print(title)
